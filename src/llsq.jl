d1aac07d6c568ad9790a49c4719e1cf837064271
Chuck Witt <witt.chuck@gmail.com>
2022-05-06 13:12:28 +0100

    work in progress commit for llsq.

--- src/llsq.jl
+++ src/llsq.jl
@@ -45,12 +45,12 @@ end
 
 function llsq(basis, data::AbstractVector, par = :serial; solver = QR())
 
-to = TimerOutput()
-
-@timeit to "assemble" begin
    if par == :serial
       _iterate = siterate
       A, y, w = asm_llsq(basis, data, _iterate)
+      coef = solve_llsq(solver, A, y)
+      config_errors = error_llsq(data, convert(Vector,A*coef)./convert(Vector,w), convert(Vector,y)./convert(Vector,w))
+      return coef, config_errors
    elseif par == :mt
       _iterate = titerate
       A, y = asm_llsq(basis, data, _iterate)
@@ -60,20 +60,6 @@ to = TimerOutput()
    else 
       error("unknown assembly type")
    end
-end
-
-@timeit to "solve" begin
-   coef = solve_llsq(solver, A, y)
-end
-
-@timeit to "errors" begin
-   config_errors = error_llsq(data, convert(Vector,A*coef)./convert(Vector,w), convert(Vector,y)./convert(Vector,w))
-end
-
-show(to)
-
-   return coef, config_errors
-
 end
 
 function get_lsq_indices(data)
@@ -132,63 +118,56 @@ function asm_llsq(basis, data, _iterate)
    return A, Y, W
 end
 
-function asm_llsq_dist(basis, data, _iterate)
+function work_function(basis, data, A, Y, W)
 
-   # for now, all data is sent to all workers, should revisit
-   _, Nobs = get_lsq_indices(data)
+   row = 1
+   localrows = localindices(Y)[1]
+   function asm_lsq_inner(dat)
+      for o in observations(dat)
+         y = vec_obs(o)
+         if row>localrows[end] || (row-1+length(y))<localrows[1]
+            row += length(y)    # increment and move on, because
+            continue            # there is no local work to do
+         end
+         oB = basis_obs(typeof(o), basis, dat.config)
+         w = get_weight(o)
+         # TODO: make this an input parameter eventually
+         if hasproperty(o, :E) || hasproperty(o, :V)
+            w = w ./ sqrt(length(dat.config))
+         end
+         # fill rows of Y and A
+         for i in 1:length(y)
+            if row>=localrows[1] && row<=localrows[end]
+               localpart(Y)[row-localrows[1]+1] = w*y[i]
+               localpart(W)[row-localrows[1]+1] = w
+               for ib = 1:length(basis)
+                  localpart(A)[row-localrows[1]+1, ib] = w*vec_obs(oB[ib])[i]
+               end
+            end
+            row += 1
+         end
+      end
+      return nothing
+   end
+
+   for dat in data
+      asm_lsq_inner(dat)
+   end
+
+   return nothing
+end
+
+function asm_llsq_dist(basis, data, _iterate)
 
    println("Creating distributed design matrix with size (", Nobs, ", ", length(basis), ")")
    A = dzeros(Nobs, length(basis))
    Y = dzeros(Nobs)
    W = dzeros(Nobs)
 
-   function work_function(data)
-      to = TimerOutput()
-
-      row = 1
-      localrows = localindices(Y)[1]
-      # TODO: i0 is not used anymore, should revisit
-      function asm_lsq_inner(i0, dat)
-         for o in observations(dat)
-            y = vec_obs(o)
-            if row>localrows[end] || (row-1+length(y))<localrows[1]
-               row += length(y)    # no local work to do,
-               continue            # so increment and move on
-            end
-            oB = basis_obs(typeof(o), basis, dat.config)
-            w = get_weight(o)
-            # TODO: make this an input parameter eventually
-            if hasproperty(o, :E) || hasproperty(o, :V)
-               w = w ./ sqrt(length(dat.config))
-            end
-            # fill rows of Y and A
-            for i in 1:length(y)
-               if row>=localrows[1] && row<=localrows[end]
-                  localpart(Y)[row-localrows[1]+1] = w*y[i]
-                  localpart(W)[row-localrows[1]+1] = w
-                  for ib = 1:length(basis)
-                     ovec = vec_obs(oB[ib])
-                     localpart(A)[row-localrows[1]+1, ib] = w*ovec[i]
-                  end
-               end
-               row += 1
-            end
-         end
-         return nothing
-      end
-
-      _iterate(asm_lsq_inner, data)
-   end
-
-   @sync for w in workers()
-#      @spawnat w _iterate(asm_lsq_inner, data)
-      @spawnat w work_function(data)
-   end
-
    # send data to main process for use with any solver
-   A = convert(Array, A)
-   Y = convert(Vector, Y)
-   W = convert(Vector, W)
+   #A = convert(Array, A)
+   #Y = convert(Vector, Y)
+   #W = convert(Vector, W)
 
    return A, Y, W
 end
