51a22ef157bf99caa479a658b622767251b3d37c
Chuck Witt <witt.chuck@gmail.com>
2022-05-20 14:27:05 +0100

    add Vref dependencies.

--- src/llsq.jl
+++ src/llsq.jl
@@ -1,6 +1,5 @@
 using Distributed, DistributedArrays
 using LinearAlgebra
-using TimerOutputs
 
 # [CO] This can't be called here, otherwise can't call ACEfit from another 
 # package that doesn't have Distributed, DistributedArrays in its dependencies
@@ -38,21 +37,26 @@ end
 #    struct Distributed parameters end  struct Serial ... end and so forth 
 #    rather than those symbols :serial, :mt, :dist
 
-function llsq!(model, data::AbstractVector, par = :serial; solver = QR())
+function llsq!(model, data::AbstractVector, Vref, par = :serial; solver = QR())
    basis = get_basis(model) # should return an ACEBasis 
-   θ, errors = llsq(basis, data, par; solver = solver)
-   set_params!(model, θ)
-   return θ, errors
+   IP, errors = llsq(basis, data, Vref, par; solver = solver)
+   #set_params!(model, θ)
+   return IP, errors
 end
 
-function llsq(basis, data::AbstractVector, par = :serial; solver = QR())
+function llsq(basis, data::AbstractVector, Vref, par = :serial; solver = QR())
 
    if par == :serial
       _iterate = siterate
       A, y, w = asm_llsq(basis, data, _iterate)
-      coef = solve_llsq(solver, A, y)
-      config_errors = error_llsq(data, convert(Vector,A*coef)./convert(Vector,w), convert(Vector,y)./convert(Vector,w))
-      return coef, config_errors
+      c = solve_llsq(solver, A, y)
+      config_errors = error_llsq(data, (A*c)./w, y./w)
+      # wcw, this is the old way, should revisit
+      IP = JuLIP.MLIPs.combine(basis, c)
+      if Vref != nothing
+         IP = JuLIP.MLIPs.SumIP(Vref, IP)
+      end
+      return IP, config_errors
    elseif par == :mt
       _iterate = titerate
       A, y = asm_llsq(basis, data, _iterate)
@@ -117,6 +121,7 @@ function asm_llsq(basis, data, _iterate)
 
    _iterate(asm_lsq_inner, data)
 
+
    return A, Y, W
 end
 
@@ -165,8 +170,6 @@ function _atoms_to_data(atoms, params)
 end
 
 function assemble_dist_new!(A, Y, W, params, basis)
-
- 
 
     row = 1
     localrows = localindices(Y)[1]
@@ -259,152 +262,6 @@ function asm_llsq_dist(basis, data, _iterate)
    #W = convert(Vector, W)
 
    return A, Y, W
-end
-
-function error_llsq(data, approx, exact)
-
-   errors = approx - exact
-   config_types = String[]
-   config_counts = Dict("set"=>Dict("E"=>0, "F"=>0, "V"=>0))
-   config_errors = Dict("set"=>Dict("E"=>0.0, "F"=>0.0, "V"=>0.0))
-   config_norms = Dict("set"=>Dict("E"=>0.0, "F"=>0.0, "V"=>0.0))
-   for dat in data
-       if !(dat.configtype in config_types)
-          push!(config_types, dat.configtype)
-          merge!(config_counts, Dict(dat.configtype=>Dict("E"=>0,   "F"=>0,   "V"=>0)))
-          merge!(config_errors, Dict(dat.configtype=>Dict("E"=>0.0, "F"=>0.0, "V"=>0.0)))
-          merge!(config_norms, Dict(dat.configtype=>Dict("E"=>0.0, "F"=>0.0, "V"=>0.0)))
-       end
-   end
-
-   i = 1
-   for dat in data
-      for o in observations(dat)
-         obs_len = length(vec_obs(o))
-         obs_errors = errors[i:i+obs_len-1]
-         obs_values = exact[i:i+obs_len-1]
-         # TODO: we store the ref energy because it is used for the relrmse
-         #       calculation ... but does it make sense to use the total energy?
-         if hasproperty(o, :E)
-            obs_values = obs_values .+ o.E_ref
-         end
-         if hasproperty(o, :E) || hasproperty(o, :V)
-            obs_errors = obs_errors ./ length(dat.config)
-            obs_values = obs_values ./ length(dat.config)
-         end
-         obs_error = sum(obs_errors.^2)
-         obs_norm = sum(obs_values.^2)
-         if hasproperty(o, :E)
-            config_counts["set"]["E"] += obs_len
-            config_errors["set"]["E"] += obs_error
-            config_norms["set"]["E"] += obs_norm
-            config_counts[dat.configtype]["E"] += obs_len
-            config_errors[dat.configtype]["E"] += obs_error
-            config_norms[dat.configtype]["E"] += obs_norm
-         elseif hasproperty(o, :F)
-            config_counts["set"]["F"] += obs_len
-            config_errors["set"]["F"] += obs_error
-            config_norms["set"]["F"] += obs_norm
-            config_counts[dat.configtype]["F"] += obs_len
-            config_errors[dat.configtype]["F"] += obs_error
-            config_norms[dat.configtype]["F"] += obs_norm
-         elseif hasproperty(o, :V)
-            config_counts["set"]["V"] += obs_len
-            config_errors["set"]["V"] += obs_error
-            config_norms["set"]["V"] += obs_norm
-            config_counts[dat.configtype]["V"] += obs_len
-            config_errors[dat.configtype]["V"] += obs_error
-            config_norms[dat.configtype]["V"] += obs_norm
-         else
-            println("something is wrong")
-         end
-         i += obs_len
-      end
-   end
-
-   for i in keys(config_errors)
-      for j in keys(config_errors[i])
-         config_errors[i][j] = sqrt(config_errors[i][j] / config_counts[i][j])
-         config_norms[i][j] = sqrt(config_norms[i][j] / config_counts[i][j])
-         config_norms[i][j] = config_errors[i][j] / config_norms[i][j]
-      end
-   end
-
-   return Dict("rmse"=>config_errors, "relrmse"=>config_norms)
-
-end
-
-function error_llsq(data, approx, exact)
-
-   errors = approx - exact
-   config_types = String[]
-   config_counts = Dict("set"=>Dict("E"=>0, "F"=>0, "V"=>0))
-   config_errors = Dict("set"=>Dict("E"=>0.0, "F"=>0.0, "V"=>0.0))
-   config_norms = Dict("set"=>Dict("E"=>0.0, "F"=>0.0, "V"=>0.0))
-   for dat in data
-       if !(dat.configtype in config_types)
-          push!(config_types, dat.configtype)
-          merge!(config_counts, Dict(dat.configtype=>Dict("E"=>0,   "F"=>0,   "V"=>0)))
-          merge!(config_errors, Dict(dat.configtype=>Dict("E"=>0.0, "F"=>0.0, "V"=>0.0)))
-          merge!(config_norms, Dict(dat.configtype=>Dict("E"=>0.0, "F"=>0.0, "V"=>0.0)))
-       end
-   end
-
-   i = 1
-   for dat in data
-      for o in observations(dat)
-         obs_len = length(vec_obs(o))
-         obs_errors = errors[i:i+obs_len-1]
-         obs_values = exact[i:i+obs_len-1]
-         # TODO: we store the ref energy because it is used for the relrmse
-         #       calculation ... but does it make sense to use the total energy?
-         if hasproperty(o, :E)
-            obs_values = obs_values .+ o.E_ref
-         end
-         if hasproperty(o, :E) || hasproperty(o, :V)
-            obs_errors = obs_errors ./ length(dat.config)
-            obs_values = obs_values ./ length(dat.config)
-         end
-         obs_error = sum(obs_errors.^2)
-         obs_norm = sum(obs_values.^2)
-         if hasproperty(o, :E)
-            config_counts["set"]["E"] += obs_len
-            config_errors["set"]["E"] += obs_error
-            config_norms["set"]["E"] += obs_norm
-            config_counts[dat.configtype]["E"] += obs_len
-            config_errors[dat.configtype]["E"] += obs_error
-            config_norms[dat.configtype]["E"] += obs_norm
-         elseif hasproperty(o, :F)
-            config_counts["set"]["F"] += obs_len
-            config_errors["set"]["F"] += obs_error
-            config_norms["set"]["F"] += obs_norm
-            config_counts[dat.configtype]["F"] += obs_len
-            config_errors[dat.configtype]["F"] += obs_error
-            config_norms[dat.configtype]["F"] += obs_norm
-         elseif hasproperty(o, :V)
-            config_counts["set"]["V"] += obs_len
-            config_errors["set"]["V"] += obs_error
-            config_norms["set"]["V"] += obs_norm
-            config_counts[dat.configtype]["V"] += obs_len
-            config_errors[dat.configtype]["V"] += obs_error
-            config_norms[dat.configtype]["V"] += obs_norm
-         else
-            println("something is wrong")
-         end
-         i += obs_len
-      end
-   end
-
-   for i in keys(config_errors)
-      for j in keys(config_errors[i])
-         config_errors[i][j] = sqrt(config_errors[i][j] / config_counts[i][j])
-         config_norms[i][j] = sqrt(config_norms[i][j] / config_counts[i][j])
-         config_norms[i][j] = config_errors[i][j] / config_norms[i][j]
-      end
-   end
-
-   return Dict("rmse"=>config_errors, "relrmse"=>config_norms)
-
 end
 
 function error_llsq(data, approx, exact)
