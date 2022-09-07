efc74c7ba589b8c490f57d4635134fe88559b383
Chuck Witt <witt.chuck@gmail.com>
2022-09-07 12:47:27 +0100

    solve_llsq -> llsq_solve, and similar.

--- src/llsq.jl
+++ src/llsq.jl
@@ -1,12 +1,12 @@
 using LinearAlgebra
 
 function llsq(data::AbstractVector, basis; solver = QR())
-    A, Y, W = assemble_llsq(data, basis)
-    C = solve_llsq(solver, Diagonal(W)*A, Diagonal(W)*Y)
+    A, Y, W = llsq_assemble(data, basis)
+    C = llsq_solve(solver, Diagonal(W)*A, Diagonal(W)*Y)
     return A, Y, W, C
 end
 
-function assemble_llsq(data, basis)
+function llsq_assemble(data, basis)
 
    firstrow = ones(Int,length(data))
    rowcount = ones(Int,length(data))
@@ -21,14 +21,14 @@ function assemble_llsq(data, basis)
    W = zeros(size(A,1))
 
    for (i,d) in enumerate(data)
-      fill_llsq!(A, Y, W, d, basis; firstrow=firstrow[i])
+      llsq_fill!(A, Y, W, d, basis; firstrow=firstrow[i])
    end
 
    return A, Y, W
 
 end
 
-function fill_llsq!(A, Y, W, dat, basis; firstrow=1)
+function llsq_fill!(A, Y, W, dat, basis; firstrow=1)
       i1 = firstrow
       i2 = firstrow + countobservations(dat) - 1
       A[i1:i2,:] .= designmatrix(dat, basis)
