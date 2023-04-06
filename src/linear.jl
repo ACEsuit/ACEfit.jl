9b806d9bdf0a0666770fbe9e3a3d0538c04039f0
Chuck Witt <witt.chuck@gmail.com>
2023-04-06 13:28:02 +0100

    tweak assemble.

--- src/linear.jl
+++ src/linear.jl
@@ -36,27 +36,26 @@ end
 
 Base.length(d::DataPacket) = count_observations(d.data)
 
-function linear_assemble(data, basis)
+function linear_assemble(data::AbstractVector{<:AbstractData}, basis)
    @info "Assembling linear problem."
-   rows = Array{UnitRange}(undef,length(data))
+   rows = Array{UnitRange}(undef,length(data))  # row ranges for each element of data
    rows[1] = 1:count_observations(data[1])
    for i in 2:length(data)
       rows[i] = rows[i-1][end] .+ (1:count_observations(data[i]))
    end
    packets = DataPacket.(rows, data)
    sort!(packets, by=length, rev=true)
+   (nprocs() > 1) && sendto(workers(), basis=basis)
    @info "  - Creating feature matrix with size ($(rows[end][end]), $(length(basis)))."
    A = SharedArray(zeros(rows[end][end],length(basis)))
    Y = SharedArray(zeros(size(A,1)))
    W = SharedArray(zeros(size(A,1)))
    @info "  - Beginning assembly with processor count:  $(nprocs())."
-   (nprocs() > 1) && sendto(workers(), basis=basis)
    @showprogress pmap(packets) do p
       A[p.rows,:] .= feature_matrix(p.data, basis)
       Y[p.rows] .= target_vector(p.data)
       W[p.rows] .= weight_vector(p.data)
    end
    @info "  - Assembly completed."
-   flush(stdout); flush(stderr)
    return Array(A), Array(Y), Array(W)
 end
