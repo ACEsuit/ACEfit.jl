faf7d9eaa3907cf377ec2e13acd3ef5e7c6e5383
Chuck Witt <witt.chuck@gmail.com>
2023-04-19 23:30:31 +0100

    update test.

--- test/test_linearsolvers.jl
+++ test/test_linearsolvers.jl
@@ -11,84 +11,84 @@ P = Diagonal(1.0 .+ rand(Nfeat))
 
 @info(" ... QR")
 solver = ACEfit.QR()
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... regularised QR, λ = 1.0")
 solver = ACEfit.QR(lambda = 1e0, P = P)
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... regularised QR, λ = 10.0")
 solver = ACEfit.QR(lambda = 1e1, P = P)
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... RRQR, rtol = 1e-15")
 solver = ACEfit.RRQR(rtol = 1e-15, P = P)
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... RRQR, rtol = 0.5")
 solver = ACEfit.RRQR(rtol = 0.5, P = P)
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... RRQR, rtol = 0.99")
 solver = ACEfit.RRQR(rtol = 0.99, P = P)
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... LSQR")
 solver = ACEfit.LSQR(damp = 0, atol = 1e-6)
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... SKLEARN_BRR")
 solver = ACEfit.SKLEARN_BRR()
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... SKLEARN_ARD")
 solver = ACEfit.SKLEARN_ARD()
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... Bayesian Linear")
 solver = ACEfit.BL()
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... Bayesian ARD")
 solver = ACEfit.BARD()
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
 
 @info(" ... Bayesian Linear Regression SVD")
 solver = ACEfit.BayesianLinearRegressionSVD()
-results = ACEfit.linear_solve(solver, A, y)
+results = ACEfit.solve(solver, A, y)
 C = results["C"]
 @show norm(A * C - y)
 @show norm(C)
