e59e5b0d7a7912788bde4f2f95d47d82a1d97df4
Chuck Witt <witt.chuck@gmail.com>
2023-04-19 22:51:25 +0100

    format docs.

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,21 @@
 using ACEfit
 using Documenter
 
-DocMeta.setdocmeta!(ACEfit, :DocTestSetup, :(using ACEfit); recursive=true)
+DocMeta.setdocmeta!(ACEfit, :DocTestSetup, :(using ACEfit); recursive = true)
 
 makedocs(;
-    modules=[ACEfit],
-    authors="Christoph Ortner <christophortner0@gmail.com> and contributors",
-    repo="https://github.com/ACEsuit/ACEfit.jl/blob/{commit}{path}#{line}",
-    sitename="ACEfit.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://ACEsuit.github.io/ACEfit.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
-)
+         modules = [ACEfit],
+         authors = "Christoph Ortner <christophortner0@gmail.com> and contributors",
+         repo = "https://github.com/ACEsuit/ACEfit.jl/blob/{commit}{path}#{line}",
+         sitename = "ACEfit.jl",
+         format = Documenter.HTML(;
+                                  prettyurls = get(ENV, "CI", "false") == "true",
+                                  canonical = "https://ACEsuit.github.io/ACEfit.jl",
+                                  assets = String[]),
+         pages = [
+             "Home" => "index.md",
+         ])
 
 deploydocs(;
-    repo="github.com/ACEsuit/ACEfit.jl",
-    devbranch="main",
-)
+           repo = "github.com/ACEsuit/ACEfit.jl",
+           devbranch = "main")
