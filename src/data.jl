0adde6267ac4a89fda1f7a8f45f114f47f314c07
Chuck Witt <witt.chuck@gmail.com>
2023-04-19 22:02:31 +0100

    short documentation for AbstractData.

--- src/data.jl
+++ src/data.jl
@@ -1,11 +1,35 @@
-# TODO: documentation
+"""
+ACEfit users should define a type of the form:
+    UserData <: AbstractData
 
+Several functions acting on such a type should be implemented:
+    count_observations
+    feature_matrix
+    target_vector
+    weight_vector
+"""
 abstract type AbstractData end
 
+
+"""
+Returns the corresponding number of rows in the design matrix.
+"""
 function count_observations(d::AbstractData) end
 
+
+"""
+Returns the corresponding design matrix (A) entries.
+"""
 function feature_matrix(d::AbstractData) end
 
+
+"""
+Returns the corresponding target vector (Y) entries.
+"""
 function target_vector(d::AbstractData) end
 
+
+"""
+Returns the corresponding weight vector (W) entries.
+"""
 function weight_vector(d::AbstractData) end
