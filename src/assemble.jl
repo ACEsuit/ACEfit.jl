91939effea80803a35e386a5a3cf0f808039fec9
Christoph Ortner <christophortner0@gmail.com>
2023-06-21 08:21:21 -0700

    multi-threaded assembly

--- src/assemble.jl
+++ src/assemble.jl
@@ -2,6 +2,7 @@ using Distributed
 using ParallelDataTransfer
 using ProgressMeter
 using SharedArrays
+using Base.Threads: nthreads, @threads
 
 struct DataPacket{T <: AbstractData}
     rows::UnitRange
@@ -32,6 +33,46 @@ function assemble(data::AbstractVector{<:AbstractData}, basis)
         A[p.rows, :] .= feature_matrix(p.data, basis)
         Y[p.rows] .= target_vector(p.data)
         W[p.rows] .= weight_vector(p.data)
+    end
+    @info "  - Assembly completed."
+    return Array(A), Array(Y), Array(W)
+end
+
+
+"""
+Assemble feature matrix, target vector, and weight vector for given data and basis.
+"""
+function mt_assemble(data::AbstractVector{<:AbstractData}, basis)
+    @info "Multi-threaded assembly of linear problem."
+    rows = Array{UnitRange}(undef, length(data))  # row ranges for each element of data
+    rows[1] = 1:count_observations(data[1])
+    for i in 2:length(data)
+        rows[i] = rows[i - 1][end] .+ (1:count_observations(data[i]))
+    end
+    packets = DataPacket.(rows, data)
+    sort!(packets, by = length, rev = true)
+    @info "  - Creating feature matrix with size ($(rows[end][end]), $(length(basis)))."
+    A = zeros(rows[end][end], length(basis))
+    Y = zeros(size(A, 1))
+    W = zeros(size(A, 1))
+    @info "  - Beginning assembly with $(Threads.nthreads()) threads."
+    _lock = ReentrantLock()
+    _prog = Progress(sum(length, rows))
+    _prog_ctr = 0
+    Threads.@threads for _i = 1:length(packets)
+        p = packets[_i]
+        Ap = feature_matrix(p.data, basis)
+        Yp = target_vector(p.data)
+        Wp = weight_vector(p.data)
+        # ----- syncronized communication block
+        lock(_lock)
+        A[p.rows, :] .= Ap 
+        Y[p.rows] .= Yp
+        W[p.rows] .= Wp
+        _prog_ctr += length(p.rows)
+        ProgressMeter.update!(_prog, _prog_ctr)
+        unlock(_lock)
+        # -----
     end
     @info "  - Assembly completed."
     return Array(A), Array(Y), Array(W)
