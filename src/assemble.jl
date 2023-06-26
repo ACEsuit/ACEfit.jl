de7730273f3a5965c43ef5dc8c12e8732547bdd2
Chuck Witt <witt.chuck@gmail.com>
2023-06-26 14:12:48 +0100

    add threadsx options.

--- src/assemble.jl
+++ src/assemble.jl
@@ -3,6 +3,7 @@ using ParallelDataTransfer
 using ProgressMeter
 using SharedArrays
 using ThreadedIterables
+using ThreadsX
 
 struct DataPacket{T <: AbstractData}
     rows::UnitRange
@@ -38,7 +39,7 @@ function assemble(data::AbstractVector{<:AbstractData}, basis)
     return Array(A), Array(Y), Array(W)
 end
 
-function assemble_threaded(data::AbstractVector{<:AbstractData}, basis)
+function assemble_threadediterables(data::AbstractVector{<:AbstractData}, basis)
     @info "Assembling linear problem."
     rows = Array{UnitRange}(undef, length(data))  # row ranges for each element of data
     rows[1] = 1:count_observations(data[1])
@@ -47,17 +48,62 @@ function assemble_threaded(data::AbstractVector{<:AbstractData}, basis)
     end
     packets = DataPacket.(rows, data)
     sort!(packets, by = length, rev = true)
-    #(nprocs() > 1) && sendto(workers(), basis = basis)
     @info "  - Creating feature matrix with size ($(rows[end][end]), $(length(basis)))."
-    A = SharedArray(zeros(rows[end][end], length(basis)))
-    Y = SharedArray(zeros(size(A, 1)))
-    W = SharedArray(zeros(size(A, 1)))
+    A = Array(zeros(rows[end][end], length(basis)))
+    Y = Array(zeros(size(A, 1)))
+    W = Array(zeros(size(A, 1)))
     @info "  - Beginning assembly with thread count:  $(Threads.nthreads())."
-    tmap(packets) do p
+    @time tmap(packets) do p
         A[p.rows, :] .= feature_matrix(p.data, basis)
         Y[p.rows] .= target_vector(p.data)
         W[p.rows] .= weight_vector(p.data)
     end
     @info "  - Assembly completed."
-    return Array(A), Array(Y), Array(W)
+    return A, Y, W
+end
+
+function assemble_threadsx(data::AbstractVector{<:AbstractData}, basis)
+    @info "Assembling linear problem."
+    rows = Array{UnitRange}(undef, length(data))  # row ranges for each element of data
+    rows[1] = 1:count_observations(data[1])
+    for i in 2:length(data)
+        rows[i] = rows[i - 1][end] .+ (1:count_observations(data[i]))
+    end
+    packets = DataPacket.(rows, data)
+    sort!(packets, by = length, rev = true)
+    @info "  - Creating feature matrix with size ($(rows[end][end]), $(length(basis)))."
+    A = Array(zeros(rows[end][end], length(basis)))
+    Y = Array(zeros(size(A, 1)))
+    W = Array(zeros(size(A, 1)))
+    @info "  - Beginning assembly with thread count:  $(Threads.nthreads())."
+    @time ThreadsX.map(packets) do p
+        A[p.rows, :] .= feature_matrix(p.data, basis)
+        Y[p.rows] .= target_vector(p.data)
+        W[p.rows] .= weight_vector(p.data)
+    end
+    @info "  - Assembly completed."
+    return A, Y, W
+end
+
+function assemble_threadsx_v2(data::AbstractVector{<:AbstractData}, basis)
+    @info "Assembling linear problem."
+    rows = Array{UnitRange}(undef, length(data))  # row ranges for each element of data
+    rows[1] = 1:count_observations(data[1])
+    for i in 2:length(data)
+        rows[i] = rows[i - 1][end] .+ (1:count_observations(data[i]))
+    end
+    packets = DataPacket.(rows, data)
+    sort!(packets, by = length, rev = true)
+    @info "  - Creating feature matrix with size ($(rows[end][end]), $(length(basis)))."
+    A = Array(zeros(rows[end][end], length(basis)))
+    Y = Array(zeros(size(A, 1)))
+    W = Array(zeros(size(A, 1)))
+    @info "  - Beginning assembly with thread count:  $(Threads.nthreads())."
+    @time ThreadsX.map!(similar(packets), packets) do p
+        A[p.rows, :] .= feature_matrix(p.data, basis)
+        Y[p.rows] .= target_vector(p.data)
+        W[p.rows] .= weight_vector(p.data)
+    end
+    @info "  - Assembly completed."
+    return A, Y, W
 end
