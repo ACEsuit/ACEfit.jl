82ee211c70957d9c0dbeec68b07a4e3eac28fb18
Chuck Witt <witt.chuck@gmail.com>
2023-07-19 11:20:56 +0100

    wip.

--- src/assemble.jl
+++ src/assemble.jl
@@ -1,4 +1,5 @@
 using Distributed
+using Folds
 using ParallelDataTransfer
 using ProgressMeter
 using SharedArrays
@@ -13,7 +14,7 @@ Base.length(d::DataPacket) = count_observations(d.data)
 """
 Assemble feature matrix, target vector, and weight vector for given data and basis.
 """
-function assemble(data::AbstractVector{<:AbstractData}, basis)
+function assemble(data::AbstractVector{<:AbstractData}, basis; mode=:threaded)
     @info "Assembling linear problem."
     rows = Array{UnitRange}(undef, length(data))  # row ranges for each element of data
     rows[1] = 1:count_observations(data[1])
@@ -22,16 +23,27 @@ function assemble(data::AbstractVector{<:AbstractData}, basis)
     end
     packets = DataPacket.(rows, data)
     sort!(packets, by = length, rev = true)
-    (nprocs() > 1) && sendto(workers(), basis = basis)
     @info "  - Creating feature matrix with size ($(rows[end][end]), $(length(basis)))."
     A = SharedArray(zeros(rows[end][end], length(basis)))
     Y = SharedArray(zeros(size(A, 1)))
     W = SharedArray(zeros(size(A, 1)))
-    @info "  - Beginning assembly with processor count:  $(nprocs())."
-    @showprogress pmap(packets) do p
-        A[p.rows, :] .= feature_matrix(p.data, basis)
+    if mode == :serial
+        @info "  - Beginning serial assembly."
+    elseif mode == :threaded
+        @info "  - Beginning threaded assembly with $(Threads.nthreads()) threads."
+        map = Folds.map
+    elseif mode == :distributed
+        @info "  - Beginning distributed assembly with $(nprocs()) processes."
+        map = pmap
+        (nprocs() > 1) && sendto(workers(), basis = basis)
+    end
+    progress = Progress(length(data))
+    map(packets) do p
+        A[p.rows,:] .= feature_matrix(p.data, basis)
         Y[p.rows] .= target_vector(p.data)
         W[p.rows] .= weight_vector(p.data)
+        next!(progress)
+        GC.gc()
     end
     @info "  - Assembly completed."
     return Array(A), Array(Y), Array(W)
